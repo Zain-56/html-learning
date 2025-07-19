b863245447d1e61d077159495983247663f8a392
Zain-56 <zainshafqat127@gmail.com>
2025-07-19 11:27:45 +0500

    variable in js

---
+++ js/index.js
@@ -0,0 +1,21 @@
+console.log("Hello Javascript")
+document.writeln("Hello Javascript")
+var haesr  = 2321
+// let , var, const
+
+let header = "321543"
+header = "hsdafashgdf"
+console.log(header)
+
+
+var navbar = "rewtry"
+var navbar = 32643521
+
+navbar = true
+console.log(navbar)
+
+const section = 'ali'
+section = "hamza"
+console.log(section)
+
+//re
