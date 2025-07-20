277615fa4dd8695389d817b58cadc0daf1dda5ff
Zain-56 <zainshafqat127@gmail.com>
2025-07-20 11:24:41 +0500

    scope, hoisting

--- js/index.js
+++ js/index.js
@@ -1,21 +1,44 @@
 console.log("Hello Javascript")
 document.writeln("Hello Javascript")
-var haesr  = 2321
+var haesr = 2321
 // let , var, const
 
 let header = "321543"
 header = "hsdafashgdf"
 console.log(header)
 
-
 var navbar = "rewtry"
 var navbar = 32643521
 
-navbar = true
+navbar = false
 console.log(navbar)
 
-const section = 'ali'
-section = "hamza"
-console.log(section)
+// const section = 'ali'
+// section = "hamza"
+// console.log(section)
 
 //re
+
+//Hoisting
+
+// console.log("name = ", name1)
+
+var name1 = "ali"
+
+sum()
+// global scope
+// functional
+// block scope
+let add = 10
+// console.log("Add =", add)
+function sum() {
+    var a = 10
+    var b = 30
+    console.log("a = ", a)
+    console.log("b = ", b)
+    // console.log("name == ", name1)
+    // console.log("Add after function = ", add)
+}
+
+
+// console.log("sum", sum)
